70ee56e600fff3437f71bb061b8d3e24a6aed22d
MrHuanghanzhang <690612676@qq.com>
2015-07-28 15:17:42 +0800

    hhz first commit

---
+++ CustomBtn/HShakeButton.h
@@ -0,0 +1,13 @@
+//
+//  HShakeButton.h
+//  CustomBtn
+//
+//  Created by 黄含章 on 15/7/28.
+//  Copyright (c) 2015年 HHZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HShakeButton : UIImageView
+
+@end
---
+++ CustomBtn/HShakeButton.m
@@ -0,0 +1,21 @@
+//
+//  HShakeButton.m
+//  CustomBtn
+//
+//  Created by 黄含章 on 15/7/28.
+//  Copyright (c) 2015年 HHZ. All rights reserved.
+//
+
+#import "HShakeButton.h"
+
+@implementation HShakeButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
